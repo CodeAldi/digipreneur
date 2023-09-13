fbaf3c2a685d048c1c54bc27e53e98edcafc3497
aldi-computer-home <codealdi@gmail.com>
2023-09-13 17:17:42 +0700

    create index func

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class LandingPageController extends Controller
 {
-    //
+    // render landing page
+    function index() : View {
+        return view('landing.index');
+    }
 }
