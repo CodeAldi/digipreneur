a3a084f71184b0cc18ed3f1a747ff35d67f6d03a
aldi-computer-home <codealdi@gmail.com>
2023-09-26 11:35:07 +0700

    change username input to email input

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -39,16 +39,15 @@
                     <span class="login100-form-title p-b-49">
                         Login
                     </span>
-
                     <div class="wrap-input100 validate-input m-b-23" data-validate="Email is reauired">
                         <span class="label-input100">Email</span>
-                        <input class="input100" type="email" name="username" placeholder="Type your username">
+                        <input class="input100" type="email" name="email" placeholder="Type your email">
                         <span class="focus-input100" data-symbol="&#xf15a;"></span>
                     </div>
 
                     <div class="wrap-input100 validate-input" data-validate="Password is required">
                         <span class="label-input100">Password</span>
-                        <input class="input100" type="password" name="pass" placeholder="Type your password">
+                        <input class="input100" type="password" name="password" placeholder="Type your password">
                         <span class="focus-input100" data-symbol="&#xf190;"></span>
                     </div>
 
