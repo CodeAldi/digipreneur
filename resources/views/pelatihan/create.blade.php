89df10456b52f1b9e14179bbbcef1fc99948fb60
Aldi Rizal Chan <codealdi@gmail.com>
2023-10-18 01:02:35 +0700

    add back button

--- resources/views/pelatihan/create.blade.php
+++ resources/views/pelatihan/create.blade.php
@@ -7,30 +7,33 @@
             <form method="POST" action="#">
                 @csrf
                 <div class="row mb-3">
-                    <label class="col-sm-2 col-form-label" for="basic-default-name">Judul Materi Pelatihan</label>
+                    <label class="col-sm-2 col-form-label" for="judul">Judul Materi Pelatihan</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="basic-default-name" name="judul_materi" />
+                        <input type="text" class="form-control" id="judul" name="judul_materi" />
                     </div>
                 </div>
                 <div class="row mb-3">
-                    <label class="col-sm-2 col-form-label" for="basic-default-email">Sub-Materi</label>
+                    <label class="col-sm-2 col-form-label" for="sub_materi">Sub-Materi</label>
                     <div class="col-sm-10">
                         <div class="input-group input-group-merge">
-                            <input type="text" id="basic-default-email" class="form-control" name="sub_materi" />
+                            <input type="text" id="sub_materi" class="form-control" name="sub_materi" />
                         </div>
                     </div>
                 </div>
                 <div class="row mb-3">
-                    <label class="col-sm-2 col-form-label" for="basic-default-name">Keterangan</label>
+                    <label class="col-sm-2 col-form-label" for="keterangan">Keterangan</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="basic-default-name" name="keterangan_materi" />
+                        <input type="text" class="form-control" id="keterangan" name="keterangan_materi" />
                         {{-- <div class="form-text">You can use letters, numbers & periods</div> --}}
                     </div>
                 </div>
             
                 <input type="text" name="role" value="instruktur" readonly hidden>
-                <div class="row justify-content-end">
-                    <div class="col-sm-10">
+                <div class="row d-flex justify-content-end">
+                    <div class="col col-sm-10 col-md-2 d-flex justify-content-end">
+                        <a href="{{ route('admin.pelatihan.index') }}" class="btn btn-secondary">Back</a>
+                    </div>
+                    <div class="col col-sm-10 col-md-2">
                         <button type="submit" class="btn btn-primary">Send</button>
                     </div>
                 </div>
